cb533d8b1450e9532247ac7d07ca2c8fcbca1c36
pmeising <pmeising@student.42wolfsburg.de>
2023-02-10 16:33:56 +0100

    Basic work on C06 ex00 getting an understanding of the requirements and structure.

--- C06/ex00/ScalarConverter.cpp
+++ C06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: pmeising <pmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 11:17:53 by pmeising          #+#    #+#             */
-/*   Updated: 2023/02/10 11:26:27 by pmeising         ###   ########.fr       */
+/*   Updated: 2023/02/10 15:47:53 by pmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,57 @@
 // Example input: 
 // 'a', '2983', 39283.02' etc.
 
-int	trueType(std::string str)
+void	printToInt(std::string& str)
 {
-	
+	int	i;
+
+	i = const_cast<int>
+	std::cout << str;
+}
+
+void	printToDouble(std::string& str)
+{
+	std::cout << str;
+}
+
+void	printToFloat(std::string& str)
+{
+	std::cout << str;
+}
+
+int	trueType(std::string& str)
+{
+	int pos = str.find_first_not_of(" \t\r\n");
+	std::string		str_1 = (&str[pos]);
+	if (!(std::isalnum(str[pos])) && (str[pos] != '+') && (str[pos] != '-')) // is it's not alphanumeric or a plus or minus sign, it's not valid for this conversion.
+		return (-1);
+	if ((!std::isalpha(str[pos])) && str_1.find('f') != std::string::npos)
+		return (3);
+	else if ((!std::isalpha(str[pos])) && (str_1.find('.')) != std::string::npos)
+		return (2);
+	else if (std::isalpha(str[pos]))
+		return (0);
+	else
+		return (1);
 }
 
 void	ScalarConverter::convert(std::string str)
 {
-	int		type[4] = {0, 1, 2, 3};
+	std::string		type[4] = {"char", "int", "double", "float"}; // 0 = char 1 = int 2 = double 3 = float
 	int		true_type;
 	true_type = trueType(str);
+	std::cout << "This: " << str << " is recognized as a(n) " << type[true_type] << "\n";
+	// int		i;
+	// char	c;
+	// double	d;
+	// float	f;
+	
+	switch (true_type)
+	{
+		case 0:
+		printToInt(str);
+		printToDouble(str);
+		printToFloat(str);
+		break;
+	}
 }
--- C06/ex00/ScalarConverter.hpp
+++ C06/ex00/ScalarConverter.hpp
@@ -6,13 +6,14 @@
 /*   By: pmeising <pmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 10:16:30 by pmeising          #+#    #+#             */
-/*   Updated: 2023/02/10 11:20:22 by pmeising         ###   ########.fr       */
+/*   Updated: 2023/02/10 15:19:06 by pmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 # include <string>
+# include <iostream>
 
 
 // Misleading subject pdf. A static class itself does not exist.
