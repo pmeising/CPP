c707d0634075a74f42dc02c12186d30bb8451389
pmeising <pmeising@student.42wolfsburg.de>
2023-02-26 19:29:01 +0100

    Fixed Function template for ex01. Integrated eval sheet tests to mani.cpp

--- C07/ex01/iter.h
+++ C07/ex01/iter.h
@@ -6,7 +6,7 @@
 /*   By: pmeising <pmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 09:55:04 by pmeising          #+#    #+#             */
-/*   Updated: 2023/02/23 12:17:52 by pmeising         ###   ########.fr       */
+/*   Updated: 2023/02/26 19:25:32 by pmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,30 @@
 # include "iostream"
 #include <cstdlib>
 
+# define DEFAULT "\033[0;39m"
+# define B_RED "\033[1;31m"
+# define B_BLUE "\033[1;94m"
+# define B_YELLOW "\033[1;33m"
+# define B_GREEN "\033[1;32m"
+# define B_PINK "\033[1;35m"
+
+
 template	<class T>
-void	iter(T *ptr, unsigned int len, T ((*func)(T const &elem)))
+void	iter(T *ptr, unsigned int len, void (*f)(T const& x))
 {
 	unsigned int	i = 0;
 
 	while (i < len)
 	{
-		func(ptr[i]);
+		f(ptr[i]);
 		i++;
 	}
 }
 
 template <class T>
-void	printing(T a)
+void	printing(T const& x)
 {
-	std::cout << a << "\n";
+	std::cout << x << "\n";
 }
 
 #endif
