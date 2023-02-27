a4dba4fcf41c41a5068359976c674983fe8ec9e4
pmeising <pmeising@student.42wolfsburg.de>
2023-02-27 21:16:43 +0100

    C08

--- C08/ex02/MutantStack.hpp
+++ C08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: pmeising <pmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 20:01:38 by pmeising          #+#    #+#             */
-/*   Updated: 2023/02/23 20:52:44 by pmeising         ###   ########.fr       */
+/*   Updated: 2023/02/27 20:16:34 by pmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,48 @@
 # define B_PINK "\033[1;35m"
 
 template <typename T>
-class MutantStack : public std::stack<T> // Stack needs to be initialized with a type, that's why this has to be a template.
+class MutantStack : public std::stack<T>
 {
 	public:
 		MutantStack();
 		MutantStack(const MutantStack& obj);
 		MutantStack& operator=(const MutantStack& rhs);
 		~MutantStack();
+		
+		typedef typename std::stack<T>::container_type::iterator iterator;
 
-	private:
+		iterator begin() {return this->c.begin();} // Stack class initiated without an adapter specified, defaults to deque. Deque contains a protected member c, which is the sequence of data. This uses the standard iterators and we are able to access them through c.
+		iterator end() {return this->c.end();}
 		
 };
 
+template <typename T>
+MutantStack<T>::MutantStack()
+{
+	std::cout << B_BLUE << "Constructor call.\n" << DEFAULT;
+}
 
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack& obj)
+{
+	std::cout << B_BLUE << "Copy constructor call.\n" << DEFAULT;
+}
+
+template <typename T>
+MutantStack<T>::~MutantStack()
+{
+	std::cout << B_BLUE << "Destructor call.\n" << DEFAULT;
+}
+
+template <typename T>
+MutantStack<T>&	MutantStack<T>::operator=(const MutantStack<T>& rhs)
+{
+	std::cout << B_BLUE << "Assignment operator overload call.\n" << DEFAULT;
+}
+
+// Resources: 
+// * https://gcc.gnu.org/onlinedocs/gcc-11.2.0/libstdc++/api/a05015.html
+// * https://cplusplus.com/reference/stack/stack/?kw=stack
+// * https://cplusplus.com/reference/deque/deque/
 
 #endif
--- C08/ex02/main.cpp
+++ C08/ex02/main.cpp
@@ -6,27 +6,86 @@
 /*   By: pmeising <pmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 20:39:43 by pmeising          #+#    #+#             */
-/*   Updated: 2023/02/23 20:55:23 by pmeising         ###   ########.fr       */
+/*   Updated: 2023/02/27 21:14:09 by pmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
-void	push(int &i)
-{
-	push(i);
-}
-
 int main()
 {
-	std::stack<int>	stack;
-	stack.push(1);
-	std::cout << "Stack value: " << stack.top() << "\n";
-	stack.push(2);
-	std::cout << "Stack value: " << stack.top() << "\n";
-	stack.push(1);
-	std::cout << "Stack value: " << stack.top() << "\n";
-	stack.pop();
-	std::cout << "Stack value: " << stack.top() << "\n";
-	return (0);
+	{
+		std::stack<int>	stack;
+		stack.push(1);
+		std::cout << "Stack value: " << stack.top() << "\n";
+		stack.push(2);
+		std::cout << "Stack value: " << stack.top() << "\n";
+		stack.push(1);
+		std::cout << "Stack value: " << stack.top() << "\n";
+		stack.pop();
+		std::cout << "Stack value: " << stack.top() << "\n";
+	}
+	{
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		// std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
+	}
+	
+	{
+		std::list<int> mstack;
+		mstack.push_back(5);
+		mstack.push_back(17);
+		mstack.pop_back();
+		std::cout << mstack.size() << std::endl;
+		mstack.push_back(3);
+		mstack.push_back(5);
+		mstack.push_back(737);
+		mstack.push_back(0);
+		std::list<int>::iterator it = mstack.begin();
+		std::list<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
+	
+	{
+		std::cout << B_GREEN << "\n############ Own tests ################\n\n";
+		MutantStack<std::string>	mStack;
+		mStack.push("Hello");
+		mStack.push("World!");
+		mStack.push("What a wonderful ");
+		mStack.push("day!");
+		std::cout << mStack.top() << "\n";
+		mStack.pop();
+		std::cout << mStack.top() << "\n";
+		MutantStack<std::string>::iterator	begin = mStack.begin();
+		MutantStack<std::string>::iterator	end = mStack.end();
+		while (begin != end)
+		{
+			std::cout << B_PINK << "Content: " << *begin << "\n" << DEFAULT;
+			begin++;
+		}
+	}
+	return 0;
 }
